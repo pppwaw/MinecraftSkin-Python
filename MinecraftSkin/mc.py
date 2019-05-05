ffbd9f8c550b0c71ef51a51d75af44573bdb8d07
pppwaw <2504454577@qq.com>
2019-05-05 22:38:18 +0800

    init

---
+++ MinecraftSkin/mc.py
@@ -0,0 +1,45 @@
+import json
+import requests
+import base64
+letter = {"a": 1, "b": 0, "c": 1, "d": 0, "e": 1, "f": 0, "g": 1,
+    "h": 0, "i": 1, "j": 0, "k": 1, "l": 0, "m": 1, "n": 0,
+    "o": 1, "p": 0, "q": 1, "r": 0, "s": 1, "t": 0, "u": 1,
+    "v": 0, "w": 1, "x": 0, "y": 1, "z": 0}
+def get_uuid(playername):
+    temp=requests.get("https://api.mojang.com/users/profiles/minecraft/{0}".format(playername))
+    if temp.status_code == 200:
+        temp=json.loads(temp.text)
+        return temp["id"]
+    else:
+        return None
+def get_skin(uuid):
+    if uuid == None:
+        raise ValueError
+    profile=json.loads(requests.get("https://sessionserver.mojang.com/session/minecraft/profile/{0}".format(uuid)).text)
+    #print(profile)
+    profile=json.loads(base64.b64decode(profile["properties"][0]["value"]))
+    return profile["textures"]["SKIN"]["url"]
+def get_cape(uuid):
+    if uuid == None:
+        raise ValueError
+    profile = json.loads(requests.get("https://sessionserver.mojang.com/session/minecraft/profile/{0}".format(uuid)).text)
+
+    profile=json.loads(base64.b64decode(profile["properties"][0]["value"]))
+    try:
+        cape = profile["textures"]["CAPE"]["url"]
+    except: return None
+    else: return cape
+def get_skin_and_cape(uuid):
+    skin=get_skin(uuid)
+    cape=get_cape(uuid)
+    return json.dumps({"skin":skin,"cape":cape})
+def is_Alex(uuid):
+    return (isEven(uuid[7]) != isEven(uuid[16 + 7])) != (isEven(uuid[15]) != isEven(uuid[16 + 15]))
+def isEven(c):
+    try:
+        int(c)
+    except:
+        return (letter[c] & 1) == 1
+    else:
+        c=int(c)
+        return (c&1) == 0
---
+++ MinecraftSkin/views.py
@@ -0,0 +1,39 @@
+from MinecraftSkin import mc
+from django import http
+def get(request,name):
+    name=name.split(".")
+    uuid=mc.get_uuid(name[0])
+    if uuid == None:
+        return http.Http404()
+    else:
+        return http.HttpResponseRedirect(mc.get_skin(uuid))
+def post(request):
+    print (request.POST["get"])
+    if "uuid" in request.POST.keys():
+        uuid = request.POST["uuid"]
+    elif "name" in request.POST.keys():
+        name = request.POST["name"]
+
+    else:
+        return http.Http404()
+    if "get" not in request.POST.keys():
+        return http.Http404()
+    if request.POST["get"] == "skin":
+        try:
+            skinurl = mc.get_skin(uuid)
+        except:
+            uuid = mc.get_uuid(name)
+            skinurl = mc.get_skin(uuid)
+        finally:
+            return http.HttpResponseRedirect(skinurl)
+    elif request.POST["get"] == "cape":
+        try:
+            capeurl = mc.get_cape(uuid)
+        except:
+            uuid = mc.get_uuid(name)
+            capeurl = mc.get_cape(uuid)
+        finally:
+            if capeurl == None:
+                return http.Http404()
+            return http.HttpResponseRedirect(capeurl)
+    return http.HttpResponse()
